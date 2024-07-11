6f17d89c90c9fbc60e9dacc9d4619ef8faa27f8b
Mara-Prazzy <mara.prazzy@gmail.com>
2024-07-11 11:10:33 -0400

    Update Chat_YCMatrix_Summit.py
    deleted delim_whitespace from pd.read_csv

--- Chat_YCMatrix_Summit.py
+++ Chat_YCMatrix_Summit.py
@@ -483,7 +483,7 @@ def get_conversation_chain(vectorstore):
 
 def get_matrix_docs():
 	matrix_docs = {}
-	df = pd.read_csv(filename_matrix_docs, delim_whitespace=True, engine='python', delimiter=';', quotechar='"', verbose=True)
+	df = pd.read_csv(filename_matrix_docs, engine='python', delimiter=';', quotechar='"', verbose=True)
 	docs = df.to_dict("records")
 	for item in docs:
 		refid = item.get(KEY_REFID)
