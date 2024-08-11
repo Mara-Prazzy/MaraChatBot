85fb4e3dd7a2a8575281a8110ad853ef95f0a43d
Mara-Prazzy <mara.prazzy@gmail.com>
2024-08-10 22:43:46 -0400

    Update Chat_YCMatrix_Summit.py

--- Chat_YCMatrix_Summit.py
+++ Chat_YCMatrix_Summit.py
@@ -807,7 +807,7 @@ def main():
 
 	#login()
 
-	if 'logged_in' not is st.session_state:
+	if 'logged_in' not in st.session_state:
 		st.session_state['logged_in'] = False
 
 	if st.session_state['logged_in']:
