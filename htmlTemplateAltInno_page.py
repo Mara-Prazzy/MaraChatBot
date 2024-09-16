04c30a7379e6b485525d8abfe4cedfc07e50b17d
Mara-Prazzy <mara.prazzy@gmail.com>
2024-09-16 13:26:49 -0400

    Update htmlTemplateAltInno_page.py
    dark mode

--- htmlTemplateAltInno_page.py
+++ htmlTemplateAltInno_page.py
@@ -1,5 +1,9 @@
-pagecss = '''
-<style>
-        body {background-color: #0E1117; color: #FAFAFA;}
+pagecss =     """
+    <style>
+        .block-container {
+            padding-top: 3rem;
+            background-color: #0E1117; /* Dark background */
+            color: #FAFAFA; /* Light text */
+        }
     </style>
-'''
+    """
