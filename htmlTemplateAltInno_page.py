164a8ff1dd0745b488ddfebae25612c51b08ece5
Mara-Prazzy <mara.prazzy@gmail.com>
2024-09-16 13:28:30 -0400

    Update htmlTemplateAltInno_page.py
    back to original

--- htmlTemplateAltInno_page.py
+++ htmlTemplateAltInno_page.py
@@ -1,9 +1,7 @@
-pagecss =     """
-    <style>
-        .block-container {
-            padding-top: 3rem;
-            background-color: #0E1117; /* Dark background */
-            color: #FAFAFA; /* Light text */
-        }
-    </style>
-    """
+pagecss = '''
+<style>
+.block-container{
+    padding-top: 3rem;
+}
+</style>
+'''
